336f3cde371f1906c4adfb46dae0b144d25c4c83
SIVAPRAKASH Arun-Singh <arun-singh.sivaprakash@vinci-energies.net>
2023-03-16 14:21:56 +0100

    sso removed

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./authConfig";
+// import { PublicClientApplication } from "@azure/msal-browser";
+// import { MsalProvider } from "@azure/msal-react";
+// import { msalConfig } from "./authConfig";
 
 
-const msalInstance = new PublicClientApplication(msalConfig);
+// const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MsalProvider instance={msalInstance} >
+    {/* <MsalProvider instance={msalInstance} > */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </MsalProvider>
+    {/* </MsalProvider> */}
   </React.StrictMode>
 );
