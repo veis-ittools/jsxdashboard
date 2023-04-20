28e844c1d350346fb1dcbb51e71300bc71a3a468
SIVAPRAKASH Arun-Singh <arun-singh.sivaprakash@vinci-energies.net>
2023-04-20 10:24:25 +0200

    added files

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -221,7 +221,7 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             /> 
-            <Typography
+            {/* <Typography
               variant="h6"
               color={colors.grey[300]}
               sx={{ m: "15px 0 5px 20px" }}
@@ -243,19 +243,19 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
- 
+  */}
 
 
 
-            <Typography
+            {/* <Typography
               variant="h6"
               color={colors.grey[300]}
               sx={{ m: "15px 0 5px 20px" }}
             >
               Business *inactive
-            </Typography>
+            </Typography> */}
 
-            <Item
+            {/* <Item
               title="Revenue"
               to="/inactive"
               icon={<BarChartOutlinedIcon />}
@@ -284,7 +284,7 @@ const Sidebar = () => {
               icon={<MapOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
-            />
+            /> */}
           </Box>
         </Menu>
       </ProSidebar>
